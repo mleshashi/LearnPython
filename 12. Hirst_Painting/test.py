32ba9960d210b9c634ae14e74026d82299c2fdec
Shashi Sharma <ss139529@gmail.com>
2024-01-21 00:54:02 +0100

    Update test.py
    used pendown and penup

--- 12. Hirst_Painting/test.py
+++ 12. Hirst_Painting/test.py
@@ -2,6 +2,13 @@ from turtle import Turtle, Screen
 
 my_turtle = Turtle()
 
-for i in range(4):
-    my_turtle.forward(100)
-    my_turtle.left(90)
+# for i in range(4):
+#     my_turtle.forward(100)
+#     my_turtle.left(90)
+
+
+for i in range(15):
+    my_turtle.forward(10)
+    my_turtle.penup()
+    my_turtle.forward(10)
+    my_turtle.pendown()
