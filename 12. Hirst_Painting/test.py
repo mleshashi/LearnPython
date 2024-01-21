0ee3c8f055a563a4b2e50e5361d88d51ce58f9fb
Shashi Sharma <ss139529@gmail.com>
2024-01-21 01:22:43 +0100

    Update test.py
    drawing different shapes

--- 12. Hirst_Painting/test.py
+++ 12. Hirst_Painting/test.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 my_turtle = Turtle()
 
@@ -13,7 +14,28 @@ my_turtle = Turtle()
 #     my_turtle.forward(10)
 #     my_turtle.pendown()
 
-my_turtle.pen(pencolor="red", pensize=5)
-for i in range(5):
-    my_turtle.forward(100)
-    my_turtle.left(72)
+
+colours = [
+    "CornflowerBlue",
+    "DarkOrchid",
+    "IndianRed",
+    "DeepSkyBlue",
+    "LightSeaGreen",
+    "wheat",
+    "SlateGray",
+    "SeaGreen",
+]
+
+my_turtle.pen(pensize=3)
+
+
+def draw_shape(number_of_side, side_length):
+    angle = 360 / number_of_side
+    for i in range(number_of_side):
+        my_turtle.forward(side_length)
+        my_turtle.left(angle)
+
+
+for number_of_side in range(3, 11):
+    my_turtle.color(random.choice(colours))
+    draw_shape(number_of_side, 100)
