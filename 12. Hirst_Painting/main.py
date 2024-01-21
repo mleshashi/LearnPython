0d89ae4f4cd6fe4d281796807d6de6df6d6f56a5
Shashi Sharma <ss139529@gmail.com>
2024-01-21 18:19:04 +0100

    Update main.py
    Complete code for the Hirst painting

--- 12. Hirst_Painting/main.py
+++ 12. Hirst_Painting/main.py
@@ -1,16 +1,76 @@
 import colorgram
+import turtle as t
+import random
 
-rgb_colors = []
+# rgb_colors = []
 
-# Using double backslashes
-path = "D:\\GitHub\\LearnPython\\12. Hirst_Painting\\"
-colors = colorgram.extract(path + "image.jpg", 30)
+# # Using double backslashes
+# path = "D:\\GitHub\\LearnPython\\12. Hirst_Painting\\"
+# colors = colorgram.extract(path + "image.jpg", 30)
 
-for color in colors:
-    r = color.rgb.r
-    g = color.rgb.g
-    b = color.rgb.b
-    new_color = (r, g, b)
-    rgb_colors.append(new_color)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
 
-print(rgb_colors)
+# print(rgb_colors)
+
+color_list = [
+    (245, 243, 238),
+    (247, 242, 244),
+    (240, 245, 241),
+    (202, 164, 109),
+    (238, 240, 245),
+    (150, 75, 49),
+    (223, 201, 135),
+    (52, 93, 124),
+    (172, 154, 40),
+    (140, 30, 19),
+    (133, 163, 185),
+    (198, 91, 71),
+    (46, 122, 86),
+    (72, 43, 35),
+    (145, 178, 148),
+    (13, 99, 71),
+    (233, 175, 164),
+    (161, 142, 158),
+    (105, 74, 77),
+    (55, 46, 50),
+    (183, 205, 171),
+    (36, 60, 74),
+    (18, 86, 90),
+    (81, 148, 129),
+    (148, 17, 20),
+    (14, 70, 64),
+    (30, 68, 100),
+    (107, 127, 153),
+    (174, 94, 97),
+    (176, 192, 209),
+]
+
+t.colormode(255)
+my_turtle = t.Turtle()
+my_turtle.speed("fastest")
+my_turtle.penup()
+my_turtle.hideturtle()
+
+my_turtle.setheading(225)
+my_turtle.forward(300)
+my_turtle.setheading(0)
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    my_turtle.dot(20, random.choice(color_list))
+    my_turtle.forward(50)
+
+    if dot_count % 10 == 0:
+        my_turtle.setheading(90)
+        my_turtle.forward(50)
+        my_turtle.setheading(180)
+        my_turtle.forward(500)
+        my_turtle.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
